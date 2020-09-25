f6641eaa01e82f206b3279dfbd25c23fd1c6e0c0
dianap125 <63566243+dianap125@users.noreply.github.com>
2020-09-25 14:54:50 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
+#imports
+from datetime import datetime
+import urllib.request
+import os
+from urllib.request import urlretrieve
+import sys
+
+#get copy to local file
+urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg476/http_access_log", "save.log")
+x = open("save.log", "r")
+URL = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+my_copy = 'save.log'
+#open/read
+x = open("save.log", "r")
+
+#vars
+total_number_request = 0
+number_of_error = 0
+number_of_redirect = 0
+date = []
+weeks = {}
+months = {}
+files = {}
+days = {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0, "Sunday": 0}
+
+
+
+
 How many requests were made on each day? 
 How many requests were made on a week-by-week basis? Per month?
 What percentage of the requests were not successful (any 4xx status code)?
